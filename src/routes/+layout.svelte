f772d519d76a5d3cac8cb25c9ab26165f512878f
jonathan earl <jonathan.earl@tylertech.com>
2024-03-24 09:29:57 -0700

    fix: directly login to google for now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,7 +21,7 @@
 		/> -->
 	{:else}
 		<a
-			href={`${base}/login?referer=${$page.data.url}`}
+			href={`${base}/auth/google/signin`}
 			style="background-color: rgb(217, 58, 0); margin-left: auto; border: none; height: 40px; text-decoration: none; display: flex; align-items: center;"
 		>
 			<span style=" padding: 0 12px; color: white; font-weight: 600; font-size: 14px;">Log In</span>
